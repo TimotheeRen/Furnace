c8c6216ed14cfe0ca8c0b36960f678a6775fa057
AnonymeUSBC <timothee.ren@proton.me>
2026-02-23 16:57:36 +0100

    ♿️ Add credentials help

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -9,6 +9,9 @@ import { Login } from "./actions/login"
 import { useTransition } from "react"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
+import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
+import { Kbd } from "@/components/ui/kbd"
+import { CodeBlock } from "@/components/ui/code-block"
 
 export default function Home() {
   const router = useRouter()
@@ -70,7 +73,31 @@ export default function Home() {
             >
               {isPending ? "Connecting..." : "Login"}
             </Button>
-            <Button variant="link" className="text-xs">Find credentials</Button>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant="link" className="text-xs">Find credentials</Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>How to find credentials</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Furnace credentials are store in the <Kbd>furnace-credentials</Kbd> secret in default namespace, as <Kbd>username</Kbd> amd <Kbd>password</Kbd>. <br/> You can get the username by typing the following command:
+                    <div className="max-w-md my-5">
+                    <CodeBlock language="bash" filename="" code="kubectl get secret furnace-credentials -o jsonpath='{.data.username}' | base64 --decode
+"></CodeBlock>
+                    </div>
+                    You can get the password by typing the following command:
+                    <div className="max-w-md mt-5">
+                    <CodeBlock language="bash" filename="" code="kubectl get secret furnace-credentials -o jsonpath='{.data.password}' | base64 --decode
+"></CodeBlock><br/>You can change them by editing the <Kbd>credentials.username</Kbd> and <Kbd>credentials.password</Kbd> Helm chart values.
+                    </div>
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>OK</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </CardFooter>
         </Card>
       </div>
