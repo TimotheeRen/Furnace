fc7bc169141b1ac999ceaafe80bf9f7b31d01e9a
AnonymeUSBC <timothee.ren@proton.me>
2026-02-23 15:34:44 +0100

    🛂 Add login middleware

---
+++ ui/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('session_token')?.value
+  const { pathname } = request.nextUrl
+  console.log("HEHO")
+  if (!token && pathname !== '/') {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
