0a35728595a0eb71bdf2ce89c33a6c8c263bde64
AnonymeUSBC <timothee.ren@proton.me>
2026-02-27 15:31:46 +0100

    💄 Make sidebar togglable

--- ui/app/dashboard/[server]/page.tsx
+++ ui/app/dashboard/[server]/page.tsx
@@ -11,9 +11,18 @@ export default async function ServerDashboard({params}: PageProps) {
   const result = await serverInfo(server)
   return (
     <>
-      <SidebarProvider defaultOpen={true}>
+      <SidebarProvider defaultOpen={false}
+      style={{
+        "--sidebar-width": "200px",         // Largeur quand elle est ouverte
+        "--sidebar-width-icon": "64px",      // Largeur quand elle est réduite (collapsed)
+      } as React.CSSProperties}
+      >
         <ServerSidebar server={server} />
+        <main className="flex-1 overflow-y-auto p-2">
+          <SidebarTrigger />
+        </main>
       </SidebarProvider>
+      <h1>t</h1>
     </>
   )
 }
