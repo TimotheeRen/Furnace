5bbdcb03d2b675975ce817fe6a1a180dcaa275ea
AnonymeUSBC <timothee.ren@proton.me>
2026-03-01 20:50:26 +0100

    💄 Fix theme flikering

--- ui/app/dashboard/[server]/layout.tsx
+++ ui/app/dashboard/[server]/layout.tsx
@@ -15,22 +15,18 @@ export default async function DashboardLayout({
   const { server } = await params;
 
   return (
-    <html lang="en">
-      <body className="antialiased">
-        <SidebarProvider 
-          defaultOpen={true}
-          style={{
-            "--sidebar-width": "200px",
-            "--sidebar-width-icon": "64px",
-          } as React.CSSProperties}
-        >
-          <ServerSidebar server={server} />
-          <main className="flex-1 overflow-y-auto p-2">
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
-      </body>
-    </html>
+    <SidebarProvider 
+      defaultOpen={true}
+      style={{
+        "--sidebar-width": "200px",
+        "--sidebar-width-icon": "64px",
+      } as React.CSSProperties}
+    >
+      <ServerSidebar server={server} />
+      <main className="flex-1 overflow-y-auto p-2">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
   );
 }
