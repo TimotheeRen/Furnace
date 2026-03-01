b92365d0f9498f5075eed9feee754d9c308e6050
AnonymeUSBC <timothee.ren@proton.me>
2026-03-01 20:14:49 +0100

    🐛 Add resilience loop

--- server/internal/jobs/console_reader.go
+++ server/internal/jobs/console_reader.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 	corev1 "k8s.io/api/core/v1"
@@ -14,43 +15,50 @@ import (
 )
 
 func ConsoleStream(ctx context.Context, rdb *redis.Client, k8sClient kubernetes.Interface, metricsClient *metricsv.Clientset) {
-	fmt.Println("Console Streaming...")
+	activeStreams := make(map[string]bool)
+	tailLines := int64(50)
+	for {
+		select {
+		case <- ctx.Done():
+			return
+		default:
+			fmt.Println("Console Streaming...")
 
-	podList, err := k8sClient.CoreV1().Pods("servers").List(ctx, metav1.ListOptions{})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+			podList, _ := k8sClient.CoreV1().Pods("servers").List(ctx, metav1.ListOptions{})
 
-	for _, pod := range podList.Items {
-		if len(pod.Spec.Containers) == 0 {
-			continue
-		}
-		tailLines := int64(50)
-		// now := metav1.Now()
-		podLogOption := corev1.PodLogOptions{
-			Container: pod.Spec.Containers[0].Name,
-			Follow: true,
-			TailLines: &tailLines,
-			// SinceTime: &now,
-		}
+			for _, pod := range podList.Items {
+				if activeStreams[pod.Name] || pod.Status.Phase != corev1.PodRunning {
+                    continue
+                }
 
-		req := k8sClient.CoreV1().Pods("servers").GetLogs(pod.Name, &podLogOption)
-		stream, err := req.Stream(ctx)
-		if err != nil {
-			fmt.Println("Stream error: ", err)
-		}
-		go func(pName string, s io.ReadCloser) {
-			if s == nil {
-				return
+                activeStreams[pod.Name] = true 
+				// now := metav1.Now()
+				podLogOption := corev1.PodLogOptions{
+					Container: pod.Spec.Containers[0].Name,
+					Follow: true,
+					TailLines: &tailLines,
+					// SinceTime: &now,
+				}
+
+				req := k8sClient.CoreV1().Pods("servers").GetLogs(pod.Name, &podLogOption)
+				stream, err := req.Stream(ctx)
+				if err != nil {
+					fmt.Println("Stream error: ", err)
+				}
+				go func(pName string, s io.ReadCloser) {
+					if s == nil {
+						return
+					}
+					defer s.Close()
+					scanner := bufio.NewScanner(s)
+					for scanner.Scan() {
+						msg := scanner.Text()
+						fmt.Printf("[%s] %s\n", pName, msg)
+						rdb.XAdd(ctx, &redis.XAddArgs{Stream: "logs:" + pName, MaxLen: 1000, Approx: true, Values: map[string]interface{}{"msg": msg}})
+					}
+				}(pod.Name, stream)
 			}
-			defer s.Close()
-			scanner := bufio.NewScanner(s)
-			for scanner.Scan() {
-				msg := scanner.Text()
-				fmt.Printf("[%s] %s\n", pName, msg)
-				rdb.XAdd(ctx, &redis.XAddArgs{Stream: "logs:" + pName, MaxLen: 1000, Approx: true, Values: map[string]interface{}{"msg": msg}})
-			}
-		}(pod.Name, stream)
+			time.Sleep(10 * time.Second)
+		}
 	}
 }
