4a1bc1fa86fc0b0c7f85692b14c5bd2fcdde9a06
AnonymeUSBC <timothee.ren@proton.me>
2026-03-07 18:30:44 +0100

    💄 Add cronjobs listing ui

--- ui/app/dashboard/[server]/cronjobs/page.tsx
+++ ui/app/dashboard/[server]/cronjobs/page.tsx
@@ -1,10 +1,24 @@
 import { getCronjobs } from "@/components/rsc/getCronjobs"
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { CodeBlock } from "@/components/ui/code-block"
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty"
+import { Item, ItemActions, ItemContent, ItemDescription, ItemMedia, ItemTitle } from "@/components/ui/item"
 import { Kbd } from "@/components/ui/kbd"
-import { FileKey } from "lucide-react"
+import { CalendarCog, FileKey, Plus, Trash2 } from "lucide-react"
+import Link from "next/link"
 
-interface PageProps {
+interface Cronjob {
+	minute: string 
+	hour: string 
+	day: string 
+	month: string 
+	week: string 
+	command: string 
+}
+
+interface PageProps
+ {
   params: Promise<{ server: string }>
 }
 
@@ -12,7 +26,56 @@ export default async function Cronjobs({params}: PageProps) {
   const { server } = await params
   const cronjobs = await getCronjobs(server)
   
+  const isEmpty = !cronjobs || cronjobs.length === 0;
+
   return (
-    <h1>Cronjobs</h1>
-  )
+    <div className="flex min-h-svh items-center justify-center">
+    {isEmpty && (
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <CalendarCog />
+          </EmptyMedia>
+          <EmptyTitle>No cronjob created</EmptyTitle>
+          <EmptyDescription>
+            You haven&#39;t created any cronjob yet.
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <Button variant="outline">
+            <Plus/>
+            Create cronjob
+          </Button>
+        </EmptyContent>
+      </Empty>
+    )}
+    {!isEmpty && (
+      <div>
+        <h1>Cronjobs</h1>
+        <div className="flex flex-col gap-2 my-3">
+          {cronjobs.map((c: Cronjob) => (
+            <Link href={`/dashboard/`} key={c.command}>
+            <Item variant="outline" className="w-100 hover:bg-zinc-900" >
+              <ItemMedia>
+                <CalendarCog />
+              </ItemMedia>
+              <ItemContent>
+                <ItemTitle>{c.command}</ItemTitle>
+                <ItemDescription>{c.minute+" "+c.hour+" "+c.day+" "+c.month+" "+c.week}</ItemDescription>
+              </ItemContent>
+              <ItemActions>
+                <Trash2/>
+              </ItemActions>
+            </Item>
+            </Link>
+          ))}
+        </div>
+          <Button variant="outline">
+            <Plus/>
+            Create cronjob
+          </Button>
+      </div>
+    )}
+    </div>
+  );
 }
